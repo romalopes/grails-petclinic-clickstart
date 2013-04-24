18cd2fde0ab9c64600566b5b14d141b93bb8b6f7
Cyrille Le Clerc <cyrille@cyrilleleclerc.com>
2013-04-25 01:42:45 +0200

    Use a JNDI datasource on production environment

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -30,20 +30,6 @@ environments {
 		}
 	}
 	production {
-		dataSource {
-			dbCreate = 'update'
-			url = 'jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000'
-			pooled = true
-			properties {
-				maxActive = -1
-				minEvictableIdleTimeMillis=1800000
-				timeBetweenEvictionRunsMillis=1800000
-				numTestsPerEvictionRun=3
-				testOnBorrow=true
-				testWhileIdle=true
-				testOnReturn=true
-				validationQuery='SELECT 1'
-			}
-		}
-	}
+        jndiName = "java:comp/env/jdbc/petclinic"
+    }
 }
